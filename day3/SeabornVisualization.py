b9db6928195e2e9e2c0c20081ca2722889d7e9c2
Wangchangchung <1132629383@qq.com>
2018-05-07 19:26:44 +0800

    seaborn可视化操作

--- day3/SeabornVisualization.py
+++ day3/SeabornVisualization.py
@@ -1,12 +1,11 @@
 # Author: Charse
 '''
-使用斯坦福大学的 seaboarn进行数据可视化操作
+使用斯坦福大学的seaboarn进行数据可视化操作
 '''
 import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 sns.set_style('darkgrid')
 names = [
@@ -23,40 +22,38 @@ names = [
 df = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data", sep='\s+', names=names)
 df['maker'] = df.car_name.map(lambda x: x.split()[0])
 df.origin = df.origin.map({1: 'America', 2: 'Europe', 3: 'Asia'})
-df=df.applymap(lambda x: np.nan if x == '?' else x).dropna()
+df = df.applymap(lambda x: np.nan if x == '?' else x).dropna()
 df['horsepower'] = df.horsepower.astype(float)
 print(df.head())
 
-'''
-
+# 绘制基本的图形, factorplot 折线图
 sns.factorplot(data=df, x="model_year", y="mpg")
+# 按照是哪个维度进行分析 col= 以区域作为一个维度
 sns.factorplot(data=df, x="model_year", y="mpg", col="origin")
-
+# cylinders 换成柱状图
 sns.factorplot("cylinders", data=df, col="origin", kind='bar')
 
+# 绘制散点图   横坐标为mpg
 g = sns.FacetGrid(df, col="origin")
 g.map(sns.distplot, "mpg")
 
-# 绘制散点图
+# 绘制散点图,横坐标为horsepower, 纵坐标为mpg
 g = sns.FacetGrid(df, col="origin")
 g.map(plt.scatter, "horsepower", "mpg")
 
-# 绘图的同时还做回归
+# 绘图的同时还做回归   sns.regplot
 g = sns.FacetGrid(df, col="origin")
 g.map(sns.regplot, "horsepower", "mpg")
+# 对左边的横纵坐标进行限制
 plt.xlim(0, 250)
 plt.ylim(0, 60)
 
-'''
-
-'''
 # 绘制等高线
 df['tons'] = (df.weight/2000).astype(int)
 g = sns.FacetGrid(df, col="origin", row="tons")
 g.map(sns.kdeplot, "horsepower", "mpg")
 plt.xlim(0, 250)
 plt.ylim(0, 60)
-
 
 # 按照两个维度进行绘图
 g = sns.FacetGrid(df, col="origin", row="tons")
@@ -67,7 +64,6 @@ g = sns.pairplot(df[["mpg", "horsepower", "weight", "origin"]], hue="origin", di
 for ax in g.axes.flat:
     plt.setp(ax.get_xticklabels(), rotation=45)
 
-
 # 组合绘图的时候顺便做一下回归预测
 g = sns.PairGrid(df[["mpg", "horsepower", "weight", "origin"]], hue="origin")
 g.map_upper(sns.regplot)
@@ -77,8 +73,6 @@ for ax in g.axes.flat:
     plt.setp(ax.get_xticklabels(), rotation=45)
 g.add_legend()
 g.set(alpha=0.5)
-
-'''
 
 # 联合绘图(kde等高)
 sns.jointplot("mpg", "horsepower", data=df, kind='kde')
